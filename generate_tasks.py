475a8bf59fa21f3a72082c5d9f199c530bd4f58a
jc <juan.cruz.business@gmail.com>
2025-01-11 14:10:53 -0600

    add m4 info

--- generate_tasks.py
+++ generate_tasks.py
@@ -39,18 +39,38 @@ TaskErr_t Create{core}Tasks();
 
 }} // namespace coralmicro"""
 
-def generate_core_source(tasks, core):
+def generate_core_source(tasks, core, header_filepath, header_prefix=None):
     # Collect task headers for this core
     task_headers = set()
     core_tasks = {}
+
+    # Extract filepath after "include" string, if it exists
+    # If it does, then get directory in whihc the header file is goiung to be generated and use that as prepix for task includes
+    try:
+        header_filepath = header_filepath.split("include/")[1].strip()
     
-    for task_name, task in tasks.items():
-        if task.get('Core', 'M7') == core:
-            task_name = task['TaskEntryPtr'].replace("_task", "")
-            task_headers.add(f"#include \"{task_name}_task.hh\"")
-            core_tasks[task_name] = task
+    except Exception as e:
+        pass
     
-    header_includes = "\n".join(sorted(task_headers))
+    if not header_prefix:
+        for task_name, task in tasks.items():
+            if task.get('Core') == core:
+                task_name = task['TaskEntryPtr'].replace("_task", "")
+                task_headers.add(f"#include \"{task_name}_task.hh\"")
+                core_tasks[task_name] = task
+        
+        header_includes = "\n".join(sorted(task_headers))
+
+    else:
+        for task_name, task in tasks.items():
+            if task.get('Core', 'M7') == core:
+                task_name = task['TaskEntryPtr'].replace("_task", "")
+                task_headers.add(f"#include \"{header_prefix}/{task_name}_task.hh\"")
+                core_tasks[task_name] = task
+        
+        header_includes = "\n".join(sorted(task_headers))
+
+
 
     config_entries = []
     for task_name, task in core_tasks.items():
@@ -69,7 +89,7 @@ def generate_core_source(tasks, core):
     return f"""// AUTO-GENERATED FILE FROM "scripts/generate_tasks.py"
 // EDIT AT YOUR OWN RISK.
 
-#include "task_config_{core.lower()}.hh"
+#include "{header_filepath}"
 #include <string.h>
 
 // Task implementations
@@ -121,15 +141,41 @@ TaskErr_t Create{core}Tasks() {{
 }} // namespace coralmicro"""
 
 def main():
-    if len(sys.argv) != 6:
-        print("Usage: generate_tasks.py <yaml_file> <m7_header> <m7_source> <m4_header> <m4_source>")
+    if len(sys.argv) > 8 or len(sys.argv) < 6:
+        print("Usage: generate_tasks.py <yaml_file> <m7_header> <m7_source> <m4_header> <m4_source> \n optional: [m7_header_prefix] [m4_header_prefix]")
         sys.exit(1)
 
-    yaml_filepath = sys.argv[1]
-    m7_header_filepath = sys.argv[2]
-    m7_source_filepath = sys.argv[3]
-    m4_header_filepath = sys.argv[4]
-    m4_source_filepath = sys.argv[5]
+    # We have at least 5 arguments, but may have 6 or 7
+    if len(sys.argv) == 6:
+        yaml_filepath = sys.argv[1]
+        m7_header_filepath = sys.argv[2]
+        m7_source_filepath = sys.argv[3]
+        m4_header_filepath = sys.argv[4]
+        m4_source_filepath = sys.argv[5]
+
+        m7_header_filepath_prefix = None
+        m4_header_filepath_prefix = None
+
+    elif len(sys.argv) == 7:
+        yaml_filepath = sys.argv[1]
+        m7_header_filepath = sys.argv[2]
+        m7_source_filepath = sys.argv[3]
+        m4_header_filepath = sys.argv[4]
+        m4_source_filepath = sys.argv[5]
+
+        m7_header_filepath_prefix  = sys.argv[6]
+        m4_header_filepath_prefix = None
+
+    elif len(sys.argv) == 8:
+        yaml_filepath = sys.argv[1]
+        m7_header_filepath = sys.argv[2]
+        m7_source_filepath = sys.argv[3]
+        m4_header_filepath = sys.argv[4]
+        m4_source_filepath = sys.argv[5]
+
+        m7_header_filepath_prefix  = sys.argv[6]
+        m4_header_filepath_prefix = sys.argv[7]
+
 
     try:
         with open(yaml_filepath, 'r') as stream:
@@ -144,13 +190,13 @@ def main():
         with open(m7_header_filepath, 'w') as f:
             f.write(generate_core_header("M7"))
         with open(m7_source_filepath, 'w') as f:
-            f.write(generate_core_source(tasks, "M7"))
+            f.write(generate_core_source(tasks, "M7", m7_header_filepath, m7_header_filepath_prefix))
 
         # Generate and write M4 files
         with open(m4_header_filepath, 'w') as f:
             f.write(generate_core_header("M4"))
         with open(m4_source_filepath, 'w') as f:
-            f.write(generate_core_source(tasks, "M4"))
+            f.write(generate_core_source(tasks, "M4", m4_header_filepath, m4_header_filepath_prefix))
 
     except Exception as e:
         print(f"Error generating task configuration: {str(e)}")
